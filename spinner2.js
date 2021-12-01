2897134f801df3f648068874beb0faf9611ef4f3
Suliman Fayyaz <hydrajin@gmail.com>
2021-12-01 02:41:47 -0500

    refactored with 2 for loops

---
+++ spinner2.js
@@ -0,0 +1,15 @@
+//Spinner 2 Refactored!
+
+const spinnerArr = ["|", "/", "-", "\\", "|"];
+
+let delay = 100;
+const increment = 200;
+
+for (let i = 0; i < 2; i++) {
+for (let spin of spinnerArr) {
+  setTimeout(() => {
+    process.stdout.write(`\r${spin}    `);
+  }, delay);
+  delay += increment;
+}
+}
