ade76c01869300c35236af335a3602b852708060
Suliman Fayyaz <89062295+hydrajin@users.noreply.github.com>
2023-01-02 18:38:55 -0700

    Update spinner2.js

--- spinner2.js
+++ spinner2.js
@@ -6,10 +6,10 @@ let delay = 100;
 const increment = 200;
 
 for (let i = 0; i < 2; i++) {
-for (let spin of spinnerArr) {
-  setTimeout(() => {
-    process.stdout.write(`\r${spin}    `);
-  }, delay);
-  delay += increment;
-}
+  for (let spin of spinnerArr) {
+    setTimeout(() => {
+      process.stdout.write(`\r${spin}    `);
+    }, delay);
+    delay += increment;
+  }
 }
